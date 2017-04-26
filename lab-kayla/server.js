c41696222930addb3fb2a33cf7fdfdaf96c48f3c
Kayla Asay <kdasay16@gmail.com>
2017-04-26 12:00:27 -0700

    working on tests

--- lab-kayla/server.js
+++ lab-kayla/server.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const bodyParser = require('.lib/parse.js');
+const bodyParser = require('./lib/parse.js');
 const http = require('http');
 const url = require('url');
 const queryString = require('querystring');
 const cowsay = require('cowsay');
+console.log(cowsay);
 const PORT = process.env.PORT || 3000;
 
 const server = module.exports = http.createServer(function(request, response) {
@@ -15,6 +16,7 @@ const server = module.exports = http.createServer(function(request, response) {
     if(request.url.pathname === '/cowsay') {
       bodyParser(request, function(err) {
         if(err) throw err;
+        //request.body.text check
         let message = cowsay.say({text: request.body.text});
         console.log(request.body);
         response.wrireHead(200, {'Content-Type': 'text/plain'});
@@ -33,14 +35,17 @@ const server = module.exports = http.createServer(function(request, response) {
 
   if(request.method === 'GET') {
     if(request.url.pathname === '/cowsay') {
-      bodyParser(request, function(err) {
-        if(err) throw err;
-        let message = cowsay.say({text: request.body.text});
-        console.log(request.body);
-        response.wrireHead(200, {'Content-Type': 'text/plain'});
-        response.write(message);
-        response.end();
-      });
+      let message = cowsay.say({text: request.url.query.text});
+      response.wrireHead(200, {'Content-Type': 'text/plain'});
+      response.write(message);
+      response.end();
+    } else {
+      let message = cowsay.say(
+        {text: 'bad request\ntry: localhost:3000/cowsay?text=howdy'}
+      );
+      response.writeHead(400, {'Content-Type': 'text.plain'});
+      response.write(message);
+      response.end();
     }
   }
 });
